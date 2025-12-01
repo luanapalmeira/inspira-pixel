5b17a0de2da09ffefb68ec5beef19355bd66d8b4
Luana Palmeira <luana.lp29@outlook.com>
2025-12-01 15:28:12 -0300

    Criação da estrutura de componentização + Criação do componente Header + Instalação da biblioteca iconify

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <Header />
+
+  <h2>InspiraPixel</h2>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <header>
+    <img src="../assets/logo.png" alt="Logo" />
+
+    <nav>
+      <ul>
+        <li>
+          <a href="">
+            <Icon icon="material-symbols:search" width="24" height="24" />
+          </a>
+        </li>
+
+        <li><a href="">inicio</a></li>
+
+        <li><a href="">Galeria</a></li>
+
+        <li><a href="">Favoritos</a></li>
+        
+        <li>
+          <a href="">
+            <Icon icon="material-symbols:person-outline" width="24" height="24" /></a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss"></style>
