20689afe848eba47dfd9a18fcc2406d2f76ce257
Luana Palmeira <luana.lp29@outlook.com>
2025-12-08 16:22:55 -0300

    Estilização da Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,15 +10,15 @@ import { Icon } from "@iconify/vue";
       <ul>
         <li>
           <a href="">
-            <Icon class="icone-1" icon="material-symbols:search" />
+            <Icon class="icone-1" icon="iconamoon:search-thin" />
           </a>
         </li>
-        <li><a href="">inicio</a></li>
+        <li><a href="">Início</a></li>
         <li><a href="">Galeria</a></li>
         <li><a href="">Favoritos</a></li>
         <li>
           <a href="">
-            <Icon class="icone-2" icon="material-symbols:person-outline" />
+            <Icon class="icone-2" icon="octicon:person-24" />
           </a>
         </li>
       </ul>
@@ -36,19 +36,26 @@ import { Icon } from "@iconify/vue";
     ul {
       display: flex;
       list-style: none;
-      gap: 1rem;
+      gap: 3rem;
       align-items: center;
+      justify-content: center;
 
       a {
         text-decoration: none;
-        color: #000000;
+        color: #262626;
+        font-size: 1.5rem;
+        font-weight: 400;
+
+        &:hover {
+          text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        }
 
         .icone-1 {
-          font-size: 1.5rem;  //Essees ícones estão sendo tratados como texto
+          font-size: 2rem;  //Esses ícones estão sendo tratados como texto
         }
 
         .icone-2 {
-          font-size: 1.5rem;
+          font-size: 2rem;
           color: #E1306C;
         }
       }
