7153ba003932c67a93b125b51b9426fead4c5485
Luana Palmeira <luana.lp29@outlook.com>
2025-12-08 16:38:03 -0300

    Estilização do componente Hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,7 +7,7 @@
             <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
         </div>
 
-        <img src="../assets/fotografa.png" alt="Imagem ilustrativa">
+        <img src="../assets/fotografa.png" alt="Imagem ilustrativa de uma fotógrafa">
     </section>
 </template>
 
@@ -15,20 +15,33 @@
 .hero {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    margin: 2rem;
+    justify-content: space-evenly;
+    margin: 7rem 2rem 2rem 2rem;
 
-    h2 {
-        font-size: 2rem;
+    div {
+        width: 35%;
 
-        span {
-            font-size: 3rem;
-            color: #E1306C;
+        h2 {
+            font-size: 3.75rem;
+            font-weight: 800;
+            color: #262626;
+            margin-bottom: 1.5rem;
+
+            span {
+                font-size: 5rem;
+                color: #E1306C;
+            }
+        }
+
+        p {
+            font-size: 1.25rem;
+            color: #262626;
+            font-weight: 500;
         }
     }
 
     img {
-        width: 40vw;
+        width: 27vw;
     }
 }
 </style>
