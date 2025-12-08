58620cc8f02d2b7560d1e1738141da471adc2af3
Luana Palmeira <luana.lp29@outlook.com>
2025-12-08 02:46:28 -0300

    Implementação da conexão com a API de imagens

--- src/components/Inspire.vue
+++ src/components/Inspire.vue
@@ -1,14 +1,36 @@
 <script setup>
+import { ref } from "vue";
 import Card from "./Card.vue";
+//Ele nos permite buscar dados de APIs externas de forma simples.
+import axios from "axios";
+
+// Criamos uma variável reativa que vai guardar a lista de imagens.
+const imagens = ref([]);
+
+async function carregarImagens() {
+    const resposta = await axios.get("https://picsum.photos/v2/list?page=2&limit=10");
+    imagens.value = resposta.data;
+    console.log(resposta);
+}
+
+carregarImagens();
 </script>
 
 <template>
     <section>
         <h2>Inspire-se</h2>
 
-        <Card imagem="https://cdn.getyourguide.com/img/location/60f693a7b5383.jpeg/150.webp" />
-        <Card imagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Aerial_view_of_Copacabana_beach.jpg/960px-Aerial_view_of_Copacabana_beach.jpg" />
+        <section class="cards">
+            <Card v-for="img in imagens" :imagem="img.download_url" />
+        </section>
     </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+    .cards {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+        justify-content: center;
+    }
+</style>
