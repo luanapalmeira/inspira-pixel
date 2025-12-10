1b71889242097527c23c756e7e6d4f8394dec417
Luana Palmeira <luana.lp29@outlook.com>
2025-12-10 00:10:04 -0300

    Corrige case sensitive do Inspire.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
-import Inspire from "./components/inspire.vue";
+import Inspire from "./components/Inspire.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
